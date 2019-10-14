48e1e1f65838dd02e9e8ae22dc191f3cf2b4c5b1
Evanescewow <frightmare.wow@hotmail.co.uk>
2019-10-14 20:47:32 +0100

    Added docs

--- GUI.py
+++ GUI.py
@@ -5,61 +5,63 @@ from tkinter import messagebox
  #initialize utilities and get json data
 json_data = Utilities.GetDbData()
 
-
+##Setup fonts for the GUI
 LARGE_FONT= ("Verdana", 25)
 BUTTON_FONT= ("Arial", 15)
 
 
+##GUI Entry point
 class PayrollApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
-        
         tk.Tk.__init__(self, *args, **kwargs)
+        
+        ##setup container for widgets
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand = True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        #setup all frames for the application
         self.frames = {}
-
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (MenuPage, payslipPage, PageTwo):
 
             frame = F(container, self)
-
             self.frames[F] = frame
-
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        #select default screen
+        self.show_frame(MenuPage)
 
+    #show's specified frame
     def show_frame(self, cont):
-
         frame = self.frames[cont]
         frame.tkraise()
 
-        
-class StartPage(tk.Frame):
 
+  #menu page of the application, can access either payslip screen or expense screen
+class MenuPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
+        
+        #title
         label = tk.Label(self, text="Payroll Application!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
+        #nav button 1
         button = tk.Button(self, text="Payslip",
-                            command=lambda: controller.show_frame(PageOne),
+                            command=lambda: controller.show_frame(PayslipPage),
                             font=BUTTON_FONT)
         button.pack()
 
+        #nav button 2
         button2 = tk.Button(self, text="Expenses",
-                            command=lambda: controller.show_frame(PageTwo),
+                            command=lambda: controller.show_frame(ExpensePage),
                             font=BUTTON_FONT)
         button2.pack()
 
 
-class PageOne(tk.Frame):
-
+class PayslipPage(tk.Frame):
     #initialization of the page
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -67,13 +69,16 @@ class PageOne(tk.Frame):
         #object setup
         titleLabel = tk.Label(self, text="Payslip Management", font=LARGE_FONT)
 
+        #button to return to menu page
         menuButton = tk.Button(self, text="Menu",
-                            command=lambda: controller.show_frame(StartPage),
+                            command=lambda: controller.show_frame(MenuPage),
                             font=BUTTON_FONT)
         
+        #button to search for employee data
         searchButton = tk.Button(self, text="Search", font=BUTTON_FONT,
                                   command=lambda: self.Search(forename, surname, yearlySalery, entryBox))
         
+        #labels
         employeeLabel = tk.Label(self, text="Enter employee number", font=BUTTON_FONT)        
         entryBox = tk.Entry(self, text="test") 
         forename = tk.Label(self, text="Forename: ", font=BUTTON_FONT)
@@ -90,11 +95,13 @@ class PageOne(tk.Frame):
         searchButton.grid(row=5, sticky="W")
         menuButton.grid(row=6, sticky="S",columnspan=3, pady=(30, 0), padx=(30, 0))
         
-        
+    
+    #searches for an employee. is bound to searchButton
     def Search(self, forename, surname, yearlySalery, entryBox):
         #read data from database
-        tempEmployee = Utilities.GetEmployeeData(entryBox.get(), json_data);
+        tempEmployee = Utilities.GetEmployeeData(entryBox.get(), json_data); #recieve data
         
+        #check for corrupted return
         if tempEmployee is None:
             print("Error: Unable to locate employee in database")
             messagebox.showerror("Error!", "Unable to locate employee in database,\n check the employee number or contact a system admin")
@@ -106,20 +113,20 @@ class PageOne(tk.Frame):
         yearlySalery.configure(text="Salery: " + str(tempEmployee.salery))
         
 
-class PageTwo(tk.Frame):
-
+#Screen to add or remove expenses from a employee
+class ExpensePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Expense Management", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
         button1 = tk.Button(self, text="Menu",
-                            command=lambda: controller.show_frame(StartPage),
+                            command=lambda: controller.show_frame(MenuPage),
                             font=BUTTON_FONT)
         button1.pack(side="left")
         
 
-
+#app config and loop run
 app = PayrollApp()
 app.geometry("400x300")
 app.resizable(width=False, height=False)
