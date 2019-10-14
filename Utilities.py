0c281effad3fdfc774fc51990035aa9d2c43ba47
Evanescewow <frightmare.wow@hotmail.co.uk>
2019-10-14 17:49:56 +0100

    Added getDB method to utilities

--- Utilities.py
+++ Utilities.py
@@ -10,3 +10,29 @@ class Employee:
 		self.surname = surname
 		self.email_address = email_address
 		self.salery = salery
+  
+def GetDbData():
+    print("Loading...")
+
+    #Check for database file, if not exist create file with correct formating
+    if not os.path.exists("./dbConnectionData.json"):
+        print("Database connection data file missing!\nCreating File...")
+
+        #generate data
+        data = {
+		    'username': '',
+		    'password': '',
+		    'host': '',
+			'database_name': ''
+			}
+        
+        with open('dbConnectionData.json', 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+		#exit program
+        exit(0)
+
+
+	#Open File
+    with open("dbConnectionData.json") as json_file:
+        data = json.load(json_file)
+        return data
